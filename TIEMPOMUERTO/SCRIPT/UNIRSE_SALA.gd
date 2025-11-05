ce50e8f1f7dc3da5415a26df78b29401e93de4ec
DarkNeku <darksoul.kun@gmail.com>
2025-11-04 22:11:28 -0300

    4/11 22:11

--- TIEMPOMUERTO/SCRIPT/UNIRSE_SALA.gd
+++ TIEMPOMUERTO/SCRIPT/UNIRSE_SALA.gd
@@ -1,1 +1,7 @@
 extends Node2D
+
+func _ready():
+    $Panel/BTN_VOLVER.connect("pressed", Callable(self, "on_btn_volver_pressed"))
+
+func on_btn_volver_pressed():
+    get_tree().change_scene_to_file("res://SCENE/MAIN_MENU.tscn")
